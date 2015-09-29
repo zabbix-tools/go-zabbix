0d1a7a96d3083a460ab80e92874a8fbcfe061e30
Ryan Armstrong <ryan@cavaliercoder.com>
2015-09-29 21:14:34 +0800

    Documented Session struct

--- session.go
+++ session.go
@@ -7,12 +7,28 @@ import (
 	"net/http"
 )
 
+// A Session is an authenticated Zabbix JSON-RPC API client. It must be
+// initialized and connected with NewSession.
 type Session struct {
-	url        string
-	authToken  string
+	// url is the URL of the Zabbix JSON-RPC API (ending in `/api_jsonrpc.php`).
+	url string
+
+	// authToken is the cached authentication token returned by `user.login` and
+	// used to authenticate all API calls in this Session.
+	authToken string
+
+	// apiVersion is the software version string of the connected Zabbix API.
 	apiVersion string
 }
 
+// NewSession returns a new Session given an API connection URL and an API
+// username and password.
+//
+// An error is returned if there was an HTTP protocol error, the API credentials
+// are incorrect or if the API version is indeterminable.
+//
+// The authentication token returned by the Zabbix API server is cached to
+// authenticate all subsequent requests in this Session.
 func NewSession(url string, username string, password string) (session *Session, err error) {
 	// create session
 	session = &Session{url: url}
@@ -52,6 +68,15 @@ func (c *Session) Version() string {
 	return c.apiVersion
 }
 
+// Do sends a JSON-RPC request and returns an API Response, using connection
+// configuration defined in the parent Session.
+//
+// An error is returned if there was an HTTP protocol error, a non-200 response
+// is received, or if an error code is set is the JSON response body.
+//
+// When err is nil, resp always contains a non-nil resp.Body.
+//
+// Generally Get, Post, or PostForm will be used instead of Do.
 func (c *Session) Do(req *Request) (resp *Response, err error) {
 	// configure request
 	req.AuthToken = c.authToken
