cb0d7be66c14c6f04b60ad0f9899482ef798e3ed
Lazarenko.AN <Lazarenko.AN@parma.ru>
2019-10-03 15:27:19 +0300

    Added support Maintenance

---
+++ maintenance_json.go
@@ -0,0 +1,50 @@
+package zabbix
+
+import (
+	"fmt"
+	"time"
+)
+
+// jMaintenance is a private map for the Zabbix API Maintenance object.
+// See: https://www.zabbix.com/documentation/2.2/manual/api/reference/maintenance/object
+type jMaintenance struct {
+	MaintenanceID   string `json:"maintenanceid"`
+	Name            string `json:"name"`
+	ActiveSince     int64  `json:"active_since,string"`
+	ActiveTill      int64  `json:"active_till,string"`
+	Description     string `json:"description"`
+	MaintenanceType int    `json:"maintenance_type,string"`
+	TagsEvaltype    int    `json:"tags_evaltype,string"`
+}
+
+// jMaintenance is a private map for the Zabbix API Maintenance object.
+// See: https://www.zabbix.com/documentation/2.2/manual/api/reference/maintenance/object
+type Timeperiods struct {
+	TimeperiodType int `json:"timeperiod_type,int"`
+	Every          int `json:"every,string"`
+	Dayofweek      int `json:"dayofweek,string"`
+	StartTime      int `json:"start_time,string"`
+	Period         int `json:"period,string"`
+}
+
+// JMaintenanceToObject returns a native Go Maintenance struct mapped from the given JSON Maintenance
+// data.
+func (c *jMaintenance) Maintenance() (result *Maintenance, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			err = fmt.Errorf("%v", e)
+		}
+	}()
+
+	maintenance := &Maintenance{
+		ActionEvalTypeAndOr: TagsEvaltype(c.MaintenanceType),
+		Type:                MaintenanceType(c.TagsEvaltype),
+		ActiveSince:         time.Unix(c.ActiveSince, 0),
+		activeTill:          time.Unix(c.ActiveTill, 0),
+		Description:         c.Description,
+		MaintenanceID:       c.MaintenanceID,
+		Name:                c.Name,
+	}
+
+	return maintenance, nil
+}
